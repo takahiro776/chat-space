5821a7c956e3e2b6cfedaf13a8c0f20976431bb8
takahiro776 <homareendo@gmail.com>
2020-05-08 15:01:38 +0900

    change message function to Ajax

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,10 +8,11 @@ class MessagesController < ApplicationController
   end
 
   def create
-    # binding.pry
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+      respond_to do |format|
+        format.json
+      end
     else
       @messages = @group.messages.includes(:user)
       @users = @group.users
